47d1ff64de813353ffb094bd47fd9fcbba150db2
Deniz <deniztoma512@gmail.com>
2019-12-11 17:18:40 +0100

    ok

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 echo "==> Running install.sh"
 
 echo "Installing Homebrew..."
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+git clone https://github.com/Homebrew/brew $HOME/.homebrew
 
 echo "Installing dependencies from Brewfile..."
 brew tap Homebrew/bundle
